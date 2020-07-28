44cd131667635d32d4eacee32e10fa8b70c0f07d
Alvaro Batista <alvinho.jesus@hotmail.com>
2020-07-27 23:27:39 -0300

    Setup inicial do projeto

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FooterBase, FooterImg } from './styles';
+import Logo from '../../assets/img/Logo.png';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://www.alura.com.br/">
+        <img src={Logo} height="40px" alt="Logo Alura" />
+      </a>
+      <p>
+        Orgulhosamente criado por Alvaro Batista
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
